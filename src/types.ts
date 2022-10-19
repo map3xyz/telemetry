f61358b47427d9504ac640f609eae5bb88c9ac51
Amadeo Pellicce <pellicceama@users.noreply.github.com>
2022-10-19 15:48:16 +0200

    adding missing types

--- src/types.ts
+++ src/types.ts
@@ -3,7 +3,7 @@ export type TelemetryCategory = 'oss' | // users that self-host the application
                                 'map3'; // map3 hosted service
 
 type SiteAction = 'waitinglist';
-type ApiAction = 'start';
+type ApiAction = 'start' | 'db_download';
 type AppAction = 'signup' | 'api_key_generated';
 type GithubAction = 'stats' | 'db_download_count' | 'stargazers_count' | 'forks_count' | 'watchers_count';
 
